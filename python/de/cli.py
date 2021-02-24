fcd45980e6666379bde38cd376db1f2ac9f92d2b
Josh Holbrook <josh.holbrook@gmail.com>
2021-02-23 20:10:21 -0500

    Add separate type check step

--- python/de/cli.py
+++ python/de/cli.py
@@ -4,8 +4,8 @@ from typing import Any, Callable, List
 import click
 import click_log
 
-from de.logger import logger
 from de.config import Config, PROJECT_ROOT, SRC_ROOT
+from de.logger import logger
 from de.steps import fmt_step, Step, StepError, steps as _steps
 
 click_log.basic_config(logger)
@@ -46,8 +46,13 @@ def run_steps(name: str, steps: List[Step]):
     )
 
 
-format_ = run_steps("format", [["black", PROJECT_ROOT]])
-lint = run_steps("lint", [["mypy", SRC_ROOT], ["flake8", PROJECT_ROOT]])
+FORMAT_STEP: Step = ["black", PROJECT_ROOT]
+LINT_STEP: Step = ["flake8", PROJECT_ROOT]
+TYPE_CHECK_STEP: Step = ["mypy", SRC_ROOT]
+
+format_ = run_steps("format", [FORMAT_STEP])
+type_check = run_steps("type_check", [TYPE_CHECK_STEP])
+lint = run_steps("lint", [TYPE_CHECK_STEP, LINT_STEP])
 
 if __name__ == "__main__":
     cli()
